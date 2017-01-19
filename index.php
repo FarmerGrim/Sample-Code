04988651168fa69bb88ced936c81c993712f0dae
FarmerGrim <tobyhummel7@gmail.com>
2017-01-18 19:54:20 -0600

    about-us/index.php
    -Updated content to reflect recent hires.
    -added hubspot script at the end

---
+++ index.php
@@ -0,0 +1,242 @@
+<?php if (substr_count($_SERVER['HTTP_ACCEPT_ENCODING'], 'gzip')) ob_start("ob_gzhandler"); else ob_start(); ?>
+<?php include "../base.php"; ?>
+<!doctype html>
+<html>
+<head>
+
+<title>About Us: The Susan Horak Group</title>
+<meta name="viewport" content="width=device-width" />
+<meta name="keywords" content="susan, Susan Horak, The susan Horak Group, Columbia, Mo, Missouri, Columbia MO, Realtor, Real Estate, agent" />
+<meta name="description" content="The Susan Horak Group: About Us.  Learn about our team of Realtors, designers, and all the rest of our staff." />
+<meta charset="utf-8" />
+<link href='https://fonts.googleapis.com/css?family=Cabin:700%7CGreat+Vibes' rel='stylesheet'>
+<link rel="stylesheet" href="../css/susanhorak.css">
+<link rel="stylesheet" href="susanhorak-aboutshg.css">
+<? include "../favicons.php" ?>
+<script>
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-30319008-1']);
+      _gaq.push(['_trackPageview']);
+
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+</script>
+</head>
+
+<body onLoad="initialize()">
+    <div id="bodybg">
+        <?php include "../navigation.html" ?>
+        <div id="banner-top">About Us</div>
+        <div id="topbit">
+            <div id="gallerytop">
+                <div id="topbar"></div>
+                <div id="bottombar"></div>
+            </div>
+            <div id="gallerybottom">
+                <img src="../about-us/headshots/largeSusan.png" alt="Susan Horak" />
+                <img src="../about-us/headshots/largeMolly.png" alt="Molly Horak" />
+                <img src="../about-us/headshots/largeJan.png" alt="Jan Wertzberger" />
+                <img src="../about-us/headshots/largeToby.png" alt="Toby" />
+                <img src="../about-us/headshots/largeChristie.png" alt="Christie" />
+                <img src="../about-us/headshots/largeBen.png" alt="Ben" />
+                <img src="../about-us/headshots/largeShannon.png" alt="Shannon" />
+            </div>
+            <div id="map-canvas"></div>
+            <div id="copypasta">
+    <!--            <p>
+                Looking for an agent who sticks a sign in your yard and calls it a day? Try somewhere else.
+               --> <p>
+We believe experience, strong negotiating skills, being easily accessible, and the desire to help are the calling cards of a great Realtor. <a href="http://www.susanhorak.com/testimonial" style="color:#d0c8cb">Or maybe we’ve just been lucky for over 25 years</a>. Regardless, we’re committed to providing you with the best experience possible when buying or selling your home.
+        
+            </div>
+            <div id="tabs">
+                <ol id="abouttabs">
+                    <li class="tab">
+                        <a href="#Susan" class="label"><div class="thumb-opacity"></div></a>
+                        <img class="img" src="../about-us/headshots/smallSusan.png" alt="Susan Horak">
+                    </li>
+                    <li class="tab">
+                        <a href="#Jan" class="label"><div class="thumb-opacity"></div></a>
+                        <img class="img" src="../about-us/headshots/smallJan.png" alt="Jan Wertzberger">
+                    </li>
+                    <li class="tab">
+                        <a href="#Molly" class="label"><div class="thumb-opacity"></div></a>
+                        <img class="img" src="../about-us/headshots/smallMolly.png" alt="Molly Horak">
+                    </li>
+                    <li class="tab">
+                        <a href="#Toby" class="label"><div class="thumb-opacity"></div></a>
+                        <img class="img" src="../about-us/headshots/smallToby.png" alt="Toby Hummel">
+                    </li>
+                    <li class="tab">
+                        <a href="#Christie" class="label"><div class="thumb-opacity"></div></a>
+                        <img class="img" src="../about-us/headshots/smallChristie.png" alt="Christie Hunter">
+                    </li>
+                    <li class="tab">
+                        <a href="#Ben" class="label"><div class="thumb-opacity"></div></a>
+                        <img class="img" src="../about-us/headshots/smallBen.png" alt="Ben Pire">
+                    </li>
+                    <li class="tab">
+                        <a href="#Shannon" class="label"><div class="thumb-opacity"></div></a>
+                        <img class="img" src="../about-us/headshots/smallShannon.png" alt="Shannon Grenke">
+                    </li>
+                </ol>
+                <div id="Susan" class="employee">
+                    <div class="imagebox">
+                        <div class="employeename">Susan</div>
+                        <img src="../about-us/headshots/largeSusan.png" alt="Susan Horak">
+                        <div class="job">Agent</div>
+                    </div>
+                    <div class="contactinfo">
+                        <span class="bold">Office: </span><a href="tel://1-573-447-2146">(573) 447-2146</a><br>
+                        <span class="bold">Cell: </span><a href="tel://1-573-864-0160">(573) 864-0160</a><br>
+                        <span class="bold">Email: </span><a href="mailto:susan@susanhorak.com">Susan@SusanHorak.com</a>
+                    </div>
+                    <div class="bio">
+                        <p>
+                            Susan enjoys her work; you don't stay in a profession for over 25 years without a passion for it. Having seen over 3500 closing transactions she consistantly leads Columbia's real estate market year after year, and is both the #1 buyers agent and #1 listing agent. As such, she has helped more buyers and sellers than any other to realize the American dream of home ownership.
+                            <p>
+                            Believing education to be the keystone of her success, she relishes the opportunity to learn and is thrilled when offered a chance to teach.
+                            <p>
+                            She attributes her successes to a strong background in retail, marketing and grassroots political campaigning. Her favorite subjects at MU involved advertising, campaigning and negotiation. First applying these skills while working and managing Gibson's, a downtown Columbia clothing store, she soon realized her commitment to business involved long hours, hard work and perseverance.
+                    </div>
+                </div>
+                <div id="Jan" class="employee">
+                    <div class="imagebox">
+                        <div class="employeename">Jan</div>
+                        <img src="../about-us/headshots/largeJan.png" alt="Jan Wertzberger">
+                    <div class="job">Agent</div>
+                    </div>
+                    <div class="contactinfo">
+                        <span class="bold">Office: </span><a href="tel://1-573-447-2146">(573) 447-2146</a><br>
+                        <span class="bold">Cell: </span><a href="tel://1-573-864-7919">(573) 864-7919</a><br>
+                        <span class="bold">Email: </span><a href="mailto:janw@susanhorak.com">JanW@SusanHorak.com</a>
+                    </div>
+                    <div class="bio">
+                        <p>
+                            Born and raised in St. Louis, Jan now calls Columbia home. She moved to Columbia in order to allow her husband to pursue a second degree from MU.  She attended Lutheran North High School for four years, then obtained an Associates degree at Florissant Valley Community College. She's been a licensed agent since 1999.	
+                         <p>
+                            Jan got into real estate after working as Susan's Office Manager for 8 years. During that time she learned many aspects of the real estate industry. Since becoming licensed, she works as a Buyer's Agent and hosts open houses on a regular basis for The Susan Horak Group.  She enjoys working with clients, assisting them through the whole home buying process. She accommodates her clients with warmth and patience; from the first day out to closing day and after, she strives to be a full service Realtor.
+                            <p>
+                            Jan is a licensed Realtor belonging to the Columbia Board of Realtors (CBOR), the Missouri Association of Realtors (MAR) and the National Association of Realtors (NAR), and has been with Susan for over 20 years.
+                    </div>
+                </div>
+                <div id="Molly" class="employee">
+                    <div class="imagebox">
+                        <div class="employeename">Molly</div>
+                        <img src="../about-us/headshots/largeMolly.png" alt="Molly Horak">
+                        <div class="job">Agent</div>
+                    </div>
+                    <div class="contactinfo">
+                        <span class="bold">Office: </span><a href="tel://1-573-447-2146">(573) 447-2146</a><br>
+                        <span class="bold">Cell: </span><a href="tel://1-573-999-2073">(573) 999-2073</a><br>
+                        <span class="bold">Email: </span><a href="mailto:molly@susanhorak.com">Molly@SusanHorak.com</a>
+                    </div>
+                    <div class="bio">
+                        <p> Molly loves working with buyers and sellers. Molly worked in real estate marketing for years as a graphic designer before getting her license. She knows how to market a home. Molly's arts background gives her a well-trained eye, seeing opportunities in a home that others miss. Molly comes from a real estate family with three generations of Realtors on both sides, so this business runs in her blood!
+
+                        <p>Graduating in 2010 from the Maryland Institute College of Art in Baltimore Maryland, Molly is now a licensed agent working with buyers, where she puts her arts background to work helping with interior design consultations and renovations to bring the client's vision to life!
+                    </div>
+                </div>
+                <div id="Toby" class="employee">
+                    <div class="imagebox">
+                        <div class="employeename">Toby</div>
+                        <img src="../about-us/headshots/largeToby.png" alt="Toby">
+                        <div class="job">Graphic Designer</div>
+                    </div>
+                    <div class="contactinfo">
+                        <span class="bold">Office: </span><a href="tel://1-573-447-2146">(573) 447-2146</a><br>
+                        <span class="bold">Email: </span><a href="mailto:graphics@susanhorak.com">Graphics@SusanHorak.com</a>
+                    </div>
+                    <div class="bio">
+                        <p>
+                            Toby began working in the graphics department in mid-2015. A native of Mid-Missouri, he has a background in computer science. He lends his technical skills and attention to detail to the team working on photography, marketing and website management. </p>
+                    </div>
+                </div>
+                <div id="Christie" class="employee">
+                    <div class="imagebox">
+                        <div class="employeename">Cristie</div>
+                        <img src="../about-us/headshots/largeChristie.png" alt="Christie">
+                        <div class="job">Bookeeper/Rental Manager</div>
+                    </div>
+                    <div class="contactinfo">
+                        <span class="bold">Office: </span><a href="tel://1-573-447-2146">(573) 447-2146</a><br>
+                        <span class="bold">Email: </span><a href="mailto:southernproperties@susanhorak.com">SouthernProperties@SusanHorak.com</a>
+                    </div>
+                    <div class="bio">
+                        <p>
+                            Starting in early 2016, Christie brought years of rental management experience with her when she joined our team. She manages our rental properties, and keeps our books straight with her amazing accounting skills! Her joyful attitude helps make our office a fun place to work. Outside of work she loves watching her boys play sports.
+                    </div>
+                </div>
+                <div id="Ben" class="employee">
+                    <div class="imagebox">
+                        <div class="employeename">Ben</div>
+                        <img src="../about-us/headshots/largeBen.png" alt="Ben">
+                        <div class="job">Graphic Designer</div>
+                    </div>
+                    <div class="contactinfo">
+                        <span class="bold">Office: </span><a href="tel://1-573-447-2146">(573) 447-2146</a><br>
+                        <span class="bold">Email: </span><a href="mailto:graphics@susanhorak.com">Graphics@SusanHorak.com</a>
+                    </div>
+                    <div class="bio">
+                        <p>
+                            Hailing from Wisconsin, Ben quickly integrated into our team producing stellar graphics and social media posts. He brings a keen eye to the graphics team assisting with marketing listings and rentals, taking photos and video, as well as designing brochures and producing floorplans.
+                    </div>
+                </div>
+                <div id="Shannon" class="employee">
+                    <div class="imagebox">
+                        <div class="employeename">Shannon</div>
+                        <img src="../about-us/headshots/largeShannon.png" alt="Shannon">
+                        <div class="job">Administrative Assistant</div>
+                    </div>
+                    <div class="contactinfo">
+                        <span class="bold">Office: </span><a href="tel://1-573-447-2146">(573) 447-2146</a><br>
+                        <span class="bold">Email: </span><a href="mailto:Southernproperties@susanhorak.com">SouthernProperties@SusanHorak.com</a>
+                    </div>
+                    <div class="bio">
+                        <p>
+                            Shannon is a librarian at heart. She quietly goes about doing a great job answering phones, preparing paperwork, and manning our front desk. Besides keeping the office running smoothly, her awesome organizational skills make her a file-keeping machine! When she's not at work, she loves to find a cozy nook where she can enjoy reading her book.
+                    </div>
+                </div>
+            </div>
+        </div>
+        <?php include "../footer.php" ?>
+    </div>
+<script src="../js/jquery-1.11.1.min.js"></script>
+<script src="../js/jquery-ui.min.js"></script>
+<script>
+    $(window).load(function () {
+        "use strict";
+        $('#gallerybottom').cycle({
+            fx: 'fade',
+            pause: 0.5,
+            delay: 0.5,
+            sync: 0.5
+        });
+    });
+</script>
+<script>
+    $(function() {
+        $( "#tabs" ).tabs();
+      });
+</script>
+<script src="../js/googlemaps-aboutshg.js"></script>
+<script src="../js/jquery.cycle.all.js"></script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAd1rhsRqRvGjYzwc46p-0Fnwt4lv840Lo&amp"></script>
+<script>var switchTo5x=true;</script>
+<script src="https://ws.sharethis.com/button/buttons.js"></script>
+<script>stLight.options({publisher: "8d1940fa-ae86-45d0-ba21-e668c9fee77c", doNotHash: false, doNotCopy: false, hashAddressBar: false});</script>
+<!-- Start of Async HubSpot Analytics Code --> 
+  <script type="text/javascript"> 
+    (function(d,s,i,r) { 
+      if (d.getElementById(i)){return;} 
+      var n=d.createElement(s),e=d.getElementsByTagName(s)[0]; 
+      n.id=i;n.src='//js.hs-analytics.net/analytics/'+(Math.ceil(new Date()/r)*r)+'/2315717.js'; 
+      e.parentNode.insertBefore(n, e); 
+    })(document,"script","hs-analytics",300000); 
+  </script> 
+<!-- End of Async HubSpot Analytics Code -->
+</body>
+</html>
